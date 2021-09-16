f90e653cf25e6465b5d1d403136ee4abcadffc7f
asakura1041 <tshipyn1041@gmail.com>
2021-09-16 18:07:46 +0900

    tailwind css導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { getUsers } from './lib/api/users';
+import { Default } from './templates/Default'
 
 function App() {
   const [users, setUsers] = useState<any[]>([])
@@ -29,25 +29,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <ul>
-          {
-            users.map(user => <li key={user.id}> {user.name} </li>)
-          }
-        </ul>
-      </header>
+      <Default></Default>
     </div>
   );
 }
---
+++ src/templates/Default.tsx
@@ -0,0 +1,12 @@
+import { Footer } from './Footer';
+import { Header } from './Header';
+
+
+export function Default() {
+  return(
+    <>
+      <Header></Header>
+      <Footer></Footer>
+    </>
+  )
+}
